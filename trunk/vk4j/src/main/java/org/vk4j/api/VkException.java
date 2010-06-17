909932a94fc67b3e4d8eb4f6f2948ebd994d4157
Valdimir Grachev <vladimir.grachev@gmail.com>
2010-06-17 14:11:12 +0400

    Note that VkException is temporary and errors handling is unfinished has been added

--- trunk/vk4j/src/main/java/org/vk4j/api/VkException.java
+++ trunk/vk4j/src/main/java/org/vk4j/api/VkException.java
@@ -5,6 +5,8 @@ package org.vk4j.api;
  * Date: May 23, 2010
  * Time: 5:17:13 PM
  */
+
+//TODO: this is temporary solution
 public class VkException extends RuntimeException {
     public VkException(String message) {
         super(message);
